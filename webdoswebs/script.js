d2f8019bf8bede02e754507311fd16f8c001cdf1
joaovitorpr <163559039+joaovitorpr@users.noreply.github.com>
2025-04-03 09:53:40 -0300

    Add files via upload

---
+++ webdoswebs/script.js
@@ -0,0 +1,122 @@
+// if (true) {
+//     console.log("vera esta mensagem")
+// }
+
+// var umNumero = 5
+// if (umNumero == 5){
+//     console.log("verá esta mensagem");
+// }
+
+// if (umNumero == 6){
+//     console.log("não verá esta mensagem");
+// }
+
+// var umaCor = "Vermelho"
+
+// if (umaCor == "Vermelho"){
+//     console.log("a cor é vermelha");
+// } 
+// else {
+//     console.log("a cor não é vermelha");
+// }
+
+// var nomeUsuario = ("Inserir nome do usuário");
+
+// if(nomeUsuario ==""){
+//     alert("O nome do usuário não foi inserido");
+// }
+// else{
+//     alert("Nome de usuário inserido: " + nomeUsuario);
+// }
+
+// var combustivel = ("Qual combustivel você utiliza?");
+
+// if (combustivel == "GNV"){
+//     alert("O carro é movido à Gás");
+// }
+// else if(combustivel == "Gasolina"){
+//     alert("O carro é movido à Gasolina");
+// }
+// else if(combustivel == "Álcool"){
+//     alert("O carro é movido à Álcool");
+// }
+// else {
+//     alert("O carro não funciona");
+// }
+
+// var numero = prompt("Digite um número de 1 a 5.");
+// if (numero == "1"){
+//     alert("Você digitou o número UM");
+// }
+// else if (numero == "2"){
+//     alert("Você digitou o número DOIS");
+// }
+// else if (numero == "3"){
+//     alert("Você digitou o número TRÊS");
+// }
+// else if (numero == "4"){
+//     alert("Você digitou o número QUATRO");
+// }
+// else if (numero == "5"){
+//     alert("Você digitou o número CINCO");
+// }
+// else{
+//     alert("Número não solicitado.");
+// }
+
+// var numero = prompt("Digite um número entre 1 e 100");
+// if (numero <10){
+//     alert("O número que você digitou está entre 1 e 10.");
+// }
+// else if (numero < 20){
+//     alert("O número que você digitou está entre 10 e 20");
+// }
+// else if (numero < 30){
+//     alert("O número que você digitou está entre 20 e 30");
+// }
+// else if (numero < 40){
+//     alert("O número que você digitou está entre 30 e 40");
+// }
+// else if (numero < 50){
+//     alert("O número que você digitou está entre 40 e 50");
+// }
+// else if (numero < 60){
+//     alert("O número que você digitou está entre 50 e 60");
+// }
+// else if (numero < 70){
+//     alert("O número que você digitou está entre 60 e 70");
+// }
+// else if (numero < 80){
+//     alert("O número que você digitou está entre 70 e 80");
+// }
+// else if (numero < 90){
+//     alert("O número que você digitou está entre 80 e 90");
+// }
+// else if (numero <= 100){
+//     alert("O número que você digitou está entre 90 e 100");
+// }
+// else{
+//     alert("Número digitado não solicitado.")
+// }
+
+// && É O CORRESPONDENTE DO AND NO PYTHON E || EQUIVALENTE AO OR   
+
+// var nome = prompt("Inserir nome");
+
+// if ((nome == "JOAO") || (nome == "joao") || (nome == "Joao")){
+//     alert("O nome inserido é João");
+// }
+// else{
+//     alert("O nome inserido não é João.");
+// }
+
+// var nome = prompt("Inserir nome");
+// var eVazio = nome == "";
+
+// if (!eVazio){
+//     alert("nome :" + nome);
+// }
+// else {
+//     alert("Nome é vazio");
+// }
+
